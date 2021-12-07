cf82032bb63f89c2254816d456a700f8c5b9ec83
Aleksey94Dan <danilchenko.aleksey94@gmail.com>
2021-12-07 21:57:15 +0700

    update test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,46 @@
 """Prepare fixture."""
 
-
 from pathlib import Path
 
 import pytest
 
 FIXTURE_DIR = Path(__file__).resolve().parent / 'fixture'
 
-BASE_HTML = FIXTURE_DIR / 'site/index.html'
+BASE_PAGE = FIXTURE_DIR / 'site/index.html'
+BASE_IMAGE = FIXTURE_DIR / 'images/nov-21-no-shave-november-cal-640x480.jpg'
 
 BASE_URL = (
     'https://www.smashingmagazine.com/2021/10/'
     'desktop-wallpaper-calendars-november-2021/'
+)
+IMAGE_URL = (
+    'http://files.smashingmagazine.com/'
+    'wallpapers/nov-21/no-shave-november/'
+    'cal/nov-21-no-shave-november-cal-320'
+    'x480.jpg?_ga=2.186979051.295077633.16'
+    '38885607-1210454835.1632544118'
 )
 
 
 @pytest.fixture()
 def html() -> str:
     """Return html file."""
-    return BASE_HTML.read_text()
+    return BASE_PAGE.read_text()
+
+
+@pytest.fixture()
+def image() -> bytes:
+    """Return image file."""
+    return BASE_IMAGE.read_bytes()
 
 
 @pytest.fixture()
 def base_url() -> str:
-    """Return html file."""
+    """Return url of html file."""
     return BASE_URL
+
+
+@pytest.fixture()
+def image_url() -> str:
+    """Return url of image file."""
+    return IMAGE_URL
--- tests/test_scrape.py
+++ tests/test_scrape.py
@@ -4,10 +4,19 @@
 from smashing_download import scrape
 
 
-def test_get_content(html, base_url, requests_mock):
+def test_get_text(html, base_url, requests_mock):
     """Test request."""
     requests_mock.get(base_url, text=html)
 
     actual_content = scrape.get_content(base_url)  # act
 
     assert html == actual_content
+
+
+def test_get_content(image, image_url, requests_mock):
+    """Test request."""
+    requests_mock.get(image_url, content=image)
+
+    actual_content = scrape.get_content(image_url)  # act
+
+    assert image == actual_content
